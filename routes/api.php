2e41470d8b9e58e8989c5c4386be3c5a9035db85
emroz <fahimalemroz@gmail.com>
2023-08-05 13:13:48 +0600

    Tag crud complete

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('tags')->group(function () {
+    Route::get('/', [TagController::class, 'index']);
+    Route::post('/store', [TagController::class, 'store']);
+    Route::get('/{tag}', [TagController::class, 'show']);
+    Route::put('/{tag}', [TagController::class, 'update']);
+    Route::delete('/{tag}', [TagController::class, 'destroy']);
 });
